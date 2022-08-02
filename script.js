5f9ebfdae5e19bba8c892f6452fba8ba30c3ed60
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-08-02 11:07:55 -0300

    criar comida

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ snake[0]  = {
     y: 8 * box
 
 }
-let direction = "rigth";
-
+let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 +1 ) * box
+}
 
 function criarBG () {
     context.fillStyle = "Lightgreen";
-    context.fillRect(0,0,16 * box, 16 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha () {
@@ -20,6 +23,11 @@ function criarCobrinha () {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -38,7 +46,8 @@ function iniciarJogo () {
     if(snake[0].y < 0 && direction == "up") snake [0].y = 16 * box;
     
     criarBG();
-    criarCobrinha()
+    criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
